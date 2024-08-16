a54a1bf4a591482f84d51b273af8cc5e0b0f9336
Sadburritos <134071785+Sadburritos@users.noreply.github.com>
2024-08-16 11:48:06 +0500

    2

--- src/main/java/com/example/coin/dto/RateDataDto.java
+++ src/main/java/com/example/coin/dto/RateDataDto.java
@@ -4,5 +4,20 @@ import com.example.coin.entity.RateData;
 
 public class RateDataDto {
     private RateData data;
-    private long timestamp;   
+    private long timestamp;
+    
+    public RateData getData() {
+        return data;
+    }
+    public long getTimestamp() {
+        return timestamp;
+    }
+    public void setData(RateData data) {
+        this.data = data;
+    }
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }   
+
+    
 }
--- src/main/java/com/example/coin/service/RateService.java
+++ src/main/java/com/example/coin/service/RateService.java
@@ -1,25 +1,48 @@
 package com.example.coin.service;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.example.coin.dto.RateDataDto;
+import com.example.coin.entity.RateData;
+import com.example.coin.repository.CoinRepository;
 
 import reactor.core.publisher.Mono;
 
 @Service
 public class RateService {
 
+	private final CoinRepository coinRepository;
 	private final WebClient webClient;
 
-	public RateService(WebClient.Builder webClientBuilder) {
+
+	public RateService(WebClient.Builder webClientBuilder, CoinRepository coinRepository) {
+		this.coinRepository = coinRepository;
 		this.webClient = webClientBuilder.baseUrl("api.coincap.io").build();
+		
+		
+		
 	}
 
-	public Mono<RateDataDto> getBitcoinRate() {
+	public void getBitcoinRate() {
 		//TODO: Этот метод должен получать с API  RateDataDto, извлекать из него RateData, добавлять в него timestamp и сохранять в базу
-		return this.webClient.get().uri("/v2/rates/bitcoin")
-			.retrieve().bodyToMono(RateDataDto.class);
+		
+		
+
+		RateDataDto rateDataDto = this.webClient.get().uri("/v2/rates/bitcoin")
+			.retrieve().bodyToMono(RateDataDto.class).block();
+
+		RateData rateData = rateDataDto.getData();
+		
+		rateData.setTimestamp(rateDataDto.getTimestamp());
+
+		coinRepository.save(rateData);
+
+	
 	}
 
 }
